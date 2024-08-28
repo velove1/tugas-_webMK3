0bd535919b13bd47d30e7f7d402609541e581e6f
velove1 <danielleraay@gmail.com>
2024-08-28 11:07:03 +0700

    Login

---
+++ asset/js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('LoginForm').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  const correctUsername = "velove";
+  const correctPassword = "velove";
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+
+
+  if (username === correctUsername && password === correctPassword){
+    alert('Login Berhasil');
+    window.Location.href = 'index.html';
+  }else{
+    alert('Username atau password salah')
+  }
+});
